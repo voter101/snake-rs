00c78e0b672e3d1dc90ea65428fb4bc58d7cb5d8
Wiktor Mociun <voter101@gmail.com>
2024-03-13 01:22:08 +0100

    Draw simple UI – centralised game board and a boarder around it

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use crossterm::{
     event::{poll, read, Event, KeyCode},
     execute, queue,
     style::{Color, Print, SetBackgroundColor, SetForegroundColor},
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, window_size, Clear, ClearType, EnterAlternateScreen,
+    },
 };
-use std::collections::HashSet;
 use std::io::{stdout, Write};
 use std::time::Duration;
+use std::{collections::HashSet, io::Stdout};
 
 #[derive(Copy, Clone)]
 enum Direction {
@@ -220,6 +222,59 @@ fn display_board(game: &Game) -> Vec<Vec<BoardPiece>> {
     res
 }
 
+fn draw_ui(
+    terminal_size: (u16, u16),
+    game_dimensions: (u8, u8),
+    stdout: &mut Stdout,
+) -> std::io::Result<()> {
+    let game_screen_start = game_screen_starting_position(terminal_size, game_dimensions);
+
+    // Top and bottom line
+    queue!(
+        stdout,
+        SetBackgroundColor(Color::Black),
+        SetForegroundColor(Color::DarkGrey)
+    )?;
+
+    vec![
+        // Left and right border
+        (0..(game_dimensions.0 as u16))
+            .flat_map(|row_i| {
+                let row = game_screen_start.0 + row_i;
+                let first_col = game_screen_start.1 - 1;
+                let last_col = first_col + game_dimensions.1 as u16 + 1;
+
+                vec![(row, first_col), (row, last_col)]
+            })
+            .collect::<Vec<(u16, u16)>>(),
+        // Top and bottom border
+        (0..(game_dimensions.1 as u16 + 2))
+            .flat_map(|col_i| {
+                let col = game_screen_start.1 + col_i - 1;
+                let first_row = game_screen_start.0 - 1;
+                let last_row = first_row + game_dimensions.0 as u16 + 1;
+
+                vec![(first_row, col), (last_row, col)]
+            })
+            .collect::<Vec<(u16, u16)>>(),
+    ]
+    .iter()
+    .flatten()
+    .for_each(|(row, col)| queue!(stdout, MoveTo(*col, *row), Print(' ')).unwrap());
+
+    Ok(())
+}
+
+fn game_screen_starting_position(
+    terminal_size: (u16, u16),
+    game_dimensions: (u8, u8),
+) -> (u16, u16) {
+    (
+        (terminal_size.0 - game_dimensions.0 as u16) / 2,
+        (terminal_size.1 - game_dimensions.1 as u16) / 2,
+    )
+}
+
 fn main() -> std::io::Result<()> {
     let mut game = Game::new((8, 16));
 
@@ -234,6 +289,13 @@ fn main() -> std::io::Result<()> {
     )?;
 
     execute!(stdout, Clear(ClearType::All))?;
+
+    let terminal_size = window_size()?;
+    draw_ui(
+        (terminal_size.rows, terminal_size.columns),
+        game.dimensions,
+        &mut stdout,
+    )?;
 
     loop {
         if poll(Duration::from_millis(250))? {
@@ -259,10 +321,26 @@ fn main() -> std::io::Result<()> {
                 _ => {}
             }
         } else {
+            let game_screen_start = game_screen_starting_position(
+                (terminal_size.rows, terminal_size.columns),
+                game.dimensions,
+            );
+            queue!(
+                stdout,
+                SetBackgroundColor(Color::DarkGreen),
+                SetForegroundColor(Color::White)
+            )?;
             game.board_to_lines()
                 .iter()
                 .enumerate()
-                .for_each(|(i, line)| queue!(stdout, MoveTo(0, i as u16), Print(line)).unwrap());
+                .for_each(|(i, line)| {
+                    queue!(
+                        stdout,
+                        MoveTo(game_screen_start.1, i as u16 + game_screen_start.0),
+                        Print(line)
+                    )
+                    .unwrap()
+                });
 
             stdout.flush()?;
             game.tick();

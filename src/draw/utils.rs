9c8a128971ad0c2a21c666630973b18dfdb9df10
Wiktor Mociun <voter101@gmail.com>
2024-05-20 23:43:01 +0200

    Improve on full-clear experience

--- src/draw/utils.rs
+++ src/draw/utils.rs
@@ -1,10 +1,10 @@
 use crossterm::{
-    queue,
+    execute,
     terminal::{Clear, ClearType},
 };
 use std::io::Stdout;
 
 pub fn full_clear(stdout: &mut Stdout) -> std::io::Result<()> {
-    queue!(stdout, Clear(ClearType::All),)?;
+    execute!(stdout, Clear(ClearType::All), Clear(ClearType::Purge))?;
     Ok(())
 }

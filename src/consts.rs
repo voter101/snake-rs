4fb3574f76183655ce13b9ec0ec68727cee0be83
Wiktor Mociun <voter101@gmail.com>
2024-05-23 22:37:03 +0200

    Improve visuals

--- src/consts.rs
+++ src/consts.rs
@@ -2,13 +2,13 @@ use crossterm::style::Color;
 
 pub const FPS_LIMIT: u64 = 300;
 
-pub const BACKGROUND_COLOR: Color = Color::DarkGrey;
+pub const BACKGROUND_COLOR: Color = Color::AnsiValue(238);
 pub const BACKGROUND_TEXT_COLOR: Color = Color::White;
 
-pub const BOARD_FIELD_BACKGROUND_COLOR: Color = Color::DarkGreen;
+pub const BOARD_FIELD_BACKGROUND_COLOR: Color = Color::AnsiValue(52);
 pub const BOARD_FIELD_TEXT_COLOR: Color = Color::White;
 
-pub const BOARD_BORDER_COLOR: Color = Color::Black;
+pub const BOARD_BORDER_COLOR: Color = Color::AnsiValue(232);
 
-pub const FPS_COUNTER_BACKGROUND_COLOR: Color = Color::Blue;
+pub const FPS_COUNTER_BACKGROUND_COLOR: Color = Color::AnsiValue(27);
 pub const FPS_COUNTER_TEXT_COLOR: Color = Color::Black;
